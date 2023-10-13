760fcc03637902f300efe77fe696ea336802b8c1
Tejas Sodhi <sodhi.tejas@gmail.com>
2023-10-12 21:00:52 -0400

    react quiz

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## Personal Information:
 
-Full Name: Enter Your Full Name
+Full Name: Tejas Sodhi
 
-Northeastern ID: Enter your Northeastern ID
+Northeastern ID: 002783260
 
 ## Getting Started
 First, fork this repository and clone it to your local machine.
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile.js'
+//import Profile from './components/profile.js'
+import Gallery from './components/gallery.js'
+import Profile from './qcomps/profile_mistake.js'
+import Comp from './qcomps/firstcomp.js'
+import Bio from './qcomps/bios.js'
+import TodoList from './qcomps/todos.js'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <TodoList />
     </div>
   )
 }
